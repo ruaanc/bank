67829c7ab9911864d8b9dadea902ada4e87d9bde
Ruan Carlos <ruan.silva@virtus.ufcg.edu.br>
2023-10-17 23:07:20 -0300

    Added test case mapping for CheckError.

--- src/main/java/org/account/AccountService.java
+++ src/main/java/org/account/AccountService.java
@@ -1,5 +1,6 @@
 package org.account;
 
+import org.account.exceptions.InsufficientFundsException;
 import org.user.User;
 
 import java.math.BigDecimal;
@@ -12,8 +13,22 @@ public class AccountService {
                 new BigDecimal("1000.00"))), new User(2L, "User02", new Account(1001L, AccountType.CURRENT_ACCOUNT,
                 new BigDecimal("1000.00"))))", "new BigDecimal("500.00")")
     * */
+
+    /*
+    UserTest: shouldReturnInsufficientBalanceException
+    * @CheckError("transfer(new BigDecimal("500.00"), new User(1L, "User01", new Account(1000L, AccountType.CURRENT_ACCOUNT,
+                new BigDecimal("0.00"))), new User(2L, "User02", new Account(1001L, AccountType.CURRENT_ACCOUNT,
+                new BigDecimal("1000.00"))))", "InsufficientFundsException.class")
+    * */
     public BigDecimal transfer(BigDecimal totalToTransfer, User transferUser, User receivingUser) {
         Account transferUserAccount = transferUser.getAccount();
+
+        int comparisonAnswer = transferUserAccount.getTotal().compareTo(totalToTransfer);
+
+        if(comparisonAnswer < 0) {
+            throw new InsufficientFundsException();
+        }
+
         transferUserAccount.setTotal(transferUserAccount.getTotal().subtract(totalToTransfer));
 
         Account receivingUserAccount = receivingUser.getAccount();
